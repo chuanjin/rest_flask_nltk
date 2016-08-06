cb2f66ad113e5bf8384d3b001e0b688215e1c628
Chuan Jin <chuan.jin.813@gmail.com>
2016-08-06 13:57:33 +0200

    Update routes

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<path:path>")
-def home(path=''):
-    return "Welcome!"
+@app.route("/<path:url>")
+def home(url='/'):
+    return "This is the home page. Your URL is: %s " % url
+
+@app.route("/add/<int:a>/<int:b>")
+def add(a,b):
+    return "%s + %s = %s" % (a, b, a + b)
+
+
+@app.route('/about')
+def about():
+    return "This is the about page!"
 
 if __name__ == "__main__":
     app.run()
