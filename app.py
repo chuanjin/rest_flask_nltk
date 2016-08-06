49a6b1338174f0cb03c071d203f8265ad2d67f84
Chuan Jin <chuan.jin.813@gmail.com>
2016-08-06 15:12:49 +0200

    Add text template

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route("/")
 @app.route("/<path:url>")
 def home(url='/'):
     return "This is the home page. Your URL is: %s " % url
 
+
 @app.route("/add/<int:a>/<int:b>")
-def add(a,b):
+def add(a, b):
     return "%s + %s = %s" % (a, b, a + b)
 
 
@@ -15,5 +21,10 @@ def add(a,b):
 def about():
     return "This is the about page!"
 
+
+@app.route('/text', methods=['GET'])
+def text():
+    return render_template('text.html', title="Text")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
