0340afb977e0344b83f33fe53bf9f19951842806
Chuan Jin <chuan.jin.813@gmail.com>
2016-08-06 17:57:49 +0200

    Add navbar

--- app.py
+++ app.py
@@ -3,17 +3,31 @@ from flask_bootstrap import Bootstrap
 import re
 import nltk
 from collections import Counter
-
-
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 
 app = Flask(__name__)
 Bootstrap(app)
+
+nav = Nav()
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Text Counter',
+        View('Home', 'home'),
+        View('Text', 'text'),
+        View('About', 'about'),
+    )
+
+nav.init_app(app)
 
 
 @app.route("/")
 @app.route("/<path:url>")
 def home(url='/'):
-    return "This is the home page. Your URL is: %s " % url
+    #return "This is the home page. Your URL is: %s " % url
+    return render_template('index.html', title="Home")
 
 
 @app.route("/add/<int:a>/<int:b>")
@@ -23,7 +37,8 @@ def add(a, b):
 
 @app.route('/about')
 def about():
-    return "This is the about page!"
+    return render_template('about.html', title="About")
+    #return "This is the about page!"
 
 
 @app.route('/text', methods=['GET', 'POST'])
@@ -32,15 +47,12 @@ def text():
     if request.method == 'POST':
         raw = request.form.get('text')
         nltk.data.path.append('./nltk_data/')  # set the path
-        print raw
         tokens = nltk.word_tokenize(raw)
         text = nltk.Text(tokens)
         nonPunct = re.compile('.*[A-Za-z0-9].*')
         filtered = [w for w in text if nonPunct.match(w)]
         results = Counter(filtered).items()
     return render_template('text.html', title="Text", results=results)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
