a535590010d9a86717abaf1c4091bf9d7b7ba319
Chuan Jin <chuan.jin.813@gmail.com>
2016-08-06 16:23:29 +0200

    Add nltk

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import re
+import nltk
+from collections import Counter
+
 
 
 app = Flask(__name__)
@@ -22,14 +26,20 @@ def about():
     return "This is the about page!"
 
 
-@app.route('/text', methods=['GET'])
+@app.route('/text', methods=['GET', 'POST'])
 def text():
-    return render_template('text.html', title="Text")
+    if request.method == 'POST':
+        raw = request.form.get('text')
+        nltk.data.path.append('./nltk_data/')  # set the path
+        print raw
+        tokens = nltk.word_tokenize(raw)
+        text = nltk.Text(tokens)
+        nonPunct = re.compile('.*[A-Za-z0-9].*')
+        filtered = [w for w in text if nonPunct.match(w)]
+        counts = Counter(filtered)
+        print counts
 
-@app.route('/text', methods=['POST'])
-def process():
-    print request.form.get('text')
-    return redirect(url_for('text'))
+    return render_template('text.html', title="Text")
 
 
 
